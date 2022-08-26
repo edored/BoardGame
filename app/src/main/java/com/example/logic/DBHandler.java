c7d5b3ddc1c643e3586efa20a50eade1acadd1dd
dennis <ed0red86@gmail.com>
2022-08-26 12:18:28 +0200

    other commits

--- app/src/main/java/com/example/logic/DBHandler.java
+++ app/src/main/java/com/example/logic/DBHandler.java
@@ -47,22 +47,6 @@ public class DBHandler  extends SQLiteOpenHelper{
         onCreate(db);
     }
 
-    public void addNewGame(String name, String age, String genre, String minNumberOfPlayers, String maxNumberOfPlayers, String duration) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-
-        values.put(KEY_NAME, name);
-        values.put(KEY_AGE, age);
-        values.put(KEY_GENRE, genre);
-        values.put(KEY_MINNUMBEROFPLAYERS, minNumberOfPlayers);
-        values.put(KEY_MAXNUMBEROFPLAYERS, maxNumberOfPlayers);
-        values.put(KEY_DURATION, duration);
-
-        db.insert(TABLE_NAME, null, values);
-
-        db.close();
-    }
-
     public void addNewGame(BasicInformation boardGame) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
@@ -84,7 +68,7 @@ public class DBHandler  extends SQLiteOpenHelper{
         List<String> nameList = new ArrayList<>();
         SQLiteDatabase db = this.getWritableDatabase();
 
-        Cursor res = db.rawQuery( "select * from boardgames", null );
+        @SuppressLint("Recycle") Cursor res = db.rawQuery( "select * from boardgames", null );
         res.moveToFirst();
         while(!res.isAfterLast()) {
             nameList.add(res.getString(res.getColumnIndex("name")));
@@ -102,7 +86,7 @@ public class DBHandler  extends SQLiteOpenHelper{
         List<String> nameList = new ArrayList<>();
         SQLiteDatabase db = this.getWritableDatabase();
 
-        Cursor res = db.rawQuery( "select * from boardgames WHERE genre = ? OR age >= ? OR duration = ? OR (minNumberOfPlayers >= ? AND maxNumberOfPlayers <= ?)", new String [] {genre, age, duration, minNumberOfPlayers, maxNumberOfPlayers} );
+        @SuppressLint("Recycle") Cursor res = db.rawQuery( "select * from boardgames WHERE genre = ? OR age >= ? OR duration = ? OR (minNumberOfPlayers >= ? AND maxNumberOfPlayers <= ?)", new String [] {genre, age, duration, minNumberOfPlayers, maxNumberOfPlayers} );
         res.moveToFirst();
         while(!res.isAfterLast()) {
             nameList.add(res.getString(res.getColumnIndex("name")));
@@ -122,7 +106,7 @@ public class DBHandler  extends SQLiteOpenHelper{
 
         long id = position + 1;
 
-        Cursor res = db.rawQuery( "select * from boardgames WHERE _id = ?", new String [] {String.valueOf(id)} );
+        @SuppressLint("Recycle") Cursor res = db.rawQuery( "select * from boardgames WHERE _id = ?", new String [] {String.valueOf(id)} );
         res.moveToFirst();
         game.setName(res.getString(res.getColumnIndex("name")));
         game.setAge(Integer.parseInt(res.getString(res.getColumnIndex("age"))));
@@ -139,7 +123,7 @@ public class DBHandler  extends SQLiteOpenHelper{
         BasicInformation game = new BasicInformation();
         SQLiteDatabase db = this.getWritableDatabase();
 
-        Cursor res = db.rawQuery( "select * from boardgames WHERE name = ?", new String [] {String.valueOf(gameName)} );
+        @SuppressLint("Recycle") Cursor res = db.rawQuery( "select * from boardgames WHERE name = ?", new String [] {String.valueOf(gameName)} );
         res.moveToFirst();
         game.setName(res.getString(res.getColumnIndex("name")));
         game.setAge(Integer.parseInt(res.getString(res.getColumnIndex("age"))));
@@ -150,89 +134,4 @@ public class DBHandler  extends SQLiteOpenHelper{
 
         return game;
     }
-
-//    public SimpleCursorAdapter populateListViewFromDB() {
-//        SQLiteDatabase db = DBHandler.getWritableDatabase();
-//        String columns[] = {DBHandler.KEY_ROWID, DBHandler.KEY_NAME, DBHandler.KEY_DURATION};
-//        Cursor cursor = db.query(DBHandler.TABLE_NAME, columns,null, null,null, null, null, null);
-//        String[] fromFieldNames = new String[]{
-//                DBHandler.KEY_ROWID, DBHandler.KEY_NAME, DBHandler.KEY_DURATION
-//        };
-//        int[] toViewIDs = new int[]{R.id.rowid, R.id.item_name, R.id.item_duration};
-//
-//        SimpleCursorAdapter gameAdapter = new SimpleCursorAdapter(
-//                null,
-//                R.layout.single_item,
-//                cursor,
-//                fromFieldNames,
-//                toViewIDs,
-//                0
-//        );
-//        return gameAdapter;
-//    }
-
-//    public SimpleCursorAdapter populateListViewFromDB(){
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        String columns[] = {DBHandler.KEY_ROWID, DBHandler.KEY_NAME, DBHandler.KEY_GENRE, DBHandler.KEY_AGE, DBHandler.KEY_MINNUMBEROFPLAYERS, DBHandler.KEY_MAXNUMBEROFPLAYERS, DBHandler.KEY_DURATION};
-//        Cursor cursor = db.query(DBHandler.TABLE_NAME, columns,null, null,null, null, null, null);
-//        String[] fromFieldNames = new String[]{
-//                DBHandler.KEY_ROWID, DBHandler.KEY_NAME, DBHandler.KEY_GENRE, DBHandler.KEY_AGE, DBHandler.KEY_MINNUMBEROFPLAYERS, DBHandler.KEY_MAXNUMBEROFPLAYERS, DBHandler.KEY_DURATION
-//        };
-//        int[] toViewIDs = new int[]{R.id.item_id, R.id.txtName, R.id.spinGenre,  R.id.txtAge, R.id.txtMinNumberOfPlayers, R.id.txtMaxNumberOfPlayers, R.id.txtDuration};
-//        SimpleCursorAdapter contactAdapter = new SimpleCursorAdapter(
-//                DBHandler.this,
-//                R.layout.single_item,
-//                cursor,
-//                fromFieldNames,
-//                toViewIDs
-//        );
-        //return contactAdapter;
-//    }
-
-//    private static class DatabaseHelper extends SQLiteOpenHelper {
-//
-//
-//
-//        public DatabaseHelper(Context context) {
-//            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-//            this.context = context;
-//            Toast.makeText(context, "constructor called", Toast.LENGTH_SHORT).show();
-//        }
-//
-//        @Override
-//        public void onCreate(SQLiteDatabase db) {
-//            String query = "CREATE TABLE " + TABLE_NAME + " ("
-//                    + KEY_ROWID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-//                    + KEY_NAME + " TEXT,"
-//                    + KEY_AGE + " TEXT,"
-//                    + KEY_GENRE + " TEXT,"
-//                    + KEY_MINNUMBEROFPLAYERS + " TEXT,"
-//                    + KEY_MAXNUMBEROFPLAYERS + " TEXT,"
-//                    + KEY_DURATION + " TEXT)";
-//            db.execSQL(query);
-//        }
-//
-//        @Override
-//        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-//            db.execSQL(DROP_TABLE);
-//            onCreate(db);
-//        }
-//    }
-
-    //    public SimpleCursorAdapter populateListViewFromDB(){
-//        String columns[] = {DatabaseHelper.KEY_ROWID, DatabaseHelper.KEY_NAME, DatabaseHelper.KEY_GENRE, DatabaseHelper.KEY_AGE, DatabaseHelper.KEY_MINNUMBEROFPLAYERS, DatabaseHelper.KEY_MAXNUMBEROFPLAYERS, DatabaseHelper.KEY_DURATION};
-//        Cursor cursor = db.query(DatabaseHelper.TABLE_NAME, columns,null, null,null, null, null, null);
-//        String[] fromFieldNames = new String[]{
-//                DatabaseHelper.KEY_ROWID, DatabaseHelper.KEY_NAME, DatabaseHelper.KEY_GENRE, DatabaseHelper.KEY_AGE, DatabaseHelper.KEY_MINNUMBEROFPLAYERS, DatabaseHelper.KEY_MAXNUMBEROFPLAYERS, DatabaseHelper.KEY_DURATION
-//        };
-//        int[] toViewIDs = new int[]{R.id.item_id, R.id.txtName, R.id.spinGenre,  R.id.txtAge, R.id.txtMinNumberOfPlayers, R.id.txtMaxNumberOfPlayers, R.id.txtDuration};
-//        SimpleCursorAdapter contactAdapter = new SimpleCursorAdapter(
-//                context,
-//                R.layout.single_item,
-//                cursor,
-//                fromFieldNames,
-//                toViewIDs
-//        );
-//        return contactAdapter;
-//    }
 }
