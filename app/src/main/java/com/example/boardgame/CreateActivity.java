beb22dd9a2adbfecbab399313c3f57ee4543f9f4
dennis <ed0red86@gmail.com>
2022-09-01 13:52:27 +0200

    other commits

--- app/src/main/java/com/example/boardgame/CreateActivity.java
+++ app/src/main/java/com/example/boardgame/CreateActivity.java
@@ -3,7 +3,6 @@ package com.example.boardgame;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
